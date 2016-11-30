5a513363000cc92b711e599e071b9d327308dad9
undervoid <yangqifan02@hotmail.com>
2016-11-30 19:46:15 +0100

    fixed interface

--- app/src/main/java/fr/utt/adrien/helloworld/LoginActivity.java
+++ app/src/main/java/fr/utt/adrien/helloworld/LoginActivity.java
@@ -5,10 +5,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
-import android.view.Window;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextView;
 import android.widget.Toast;
 
 /**
@@ -19,8 +17,8 @@ public class LoginActivity extends AppCompatActivity {
      * The Tx.
      */
     Button btnLogin;
-    EditText tvIdentifiant;
-    EditText tvMdp;
+    EditText etIdentifiant;
+    EditText etMdp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,40 +26,41 @@ public class LoginActivity extends AppCompatActivity {
         setContentView(R.layout.activity_login);
         //requestWindowFeature(Window.FEATURE_NO_TITLE);
         btnLogin = (Button) findViewById(R.id.btn_sign_in);
-        tvIdentifiant= (EditText) findViewById(R.id.login_user_name);
-        tvMdp= (EditText) findViewById(R.id.login_user_pwd);
-        final String nom_user=tvIdentifiant.getText().toString();
-        final String mdp=tvMdp.getText().toString();
+        etIdentifiant = (EditText) findViewById(R.id.login_user_name);
+        etMdp = (EditText) findViewById(R.id.login_user_pwd);
+
         btnLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
-                boolean authen=connextionTest(nom_user,mdp);
+                boolean authen = connextionTest();
 
-                if (authen){
+                if (authen) {
                     goToTasklistActivity();
-                    Log.d("testLogin","réussi à se connecter");
-                }else{
-                    Toast.makeText(LoginActivity.this," échec de connexion",Toast.LENGTH_LONG).show();
-                    Log.d("testLogin","échouer à se connecter");
+                    Log.d("testLogin", "réussi à se connecter");
+                } else {
+                    Toast.makeText(LoginActivity.this, " échec de connexion", Toast.LENGTH_LONG).show();
+                    Log.d("testLogin", "échouer à se connecter");
                 }
             }
         });
     }
 
     private void goToTasklistActivity() {
-        Intent intent= new Intent();
-        intent.setClass(LoginActivity.this,TasklistActivity.class);
+        Intent intent = new Intent();
+        intent.setClass(LoginActivity.this, TasklistActivity.class);
         startActivity(intent);
     }
 
-    private boolean connextionTest(String nom_user,String mdp){
-//        if (nom_user.equals("admin")&&mdp.equals("123")){
-//            return true;
-//        }else {
-//            return false;
-//        }
-        return true;
+    private boolean connextionTest() {
+        String nom_user = etIdentifiant.getText().toString();
+        String mdp = etMdp.getText().toString();
+        if (nom_user.equals("admin") && mdp.equals("123")) {
+            return true;
+        } else {
+            return false;
+        }
+
     }
 
 }
