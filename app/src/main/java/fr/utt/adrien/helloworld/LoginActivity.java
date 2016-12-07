b8a4cb3402bc96da7ab57d8bf3979d516a032bc7
marioedji <edjimario@gmail.com>
2016-12-07 12:42:12 +0100

    TP03 	modified:   .idea/gradle.xml 	modified:   .idea/misc.xml 	modified:   .idea/modules.xml 	modified:   app/build.gradle 	modified:   app/src/main/AndroidManifest.xml 	modified:   app/src/main/java/fr/utt/adrien/helloworld/LoginActivity.java 	modified:   app/src/main/java/fr/utt/adrien/helloworld/TasklistActivity.java 	modified:   app/src/main/res/layout/activity_tasklist.xml 	modified:   app/src/main/res/values/styles.xml
    	app/src/main/java/fr/utt/adrien/helloworld/RequestActivity.java
    	app/src/main/java/fr/utt/adrien/helloworld/model/
    	app/src/main/res/menu/

--- app/src/main/java/fr/utt/adrien/helloworld/LoginActivity.java
+++ app/src/main/java/fr/utt/adrien/helloworld/LoginActivity.java
@@ -9,6 +9,22 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+import com.google.gson.Gson;
+
+import java.io.UnsupportedEncodingException;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.HashMap;
+import java.util.Map;
+
+import fr.utt.adrien.helloworld.model.Chauffeur;
+
 /**
  * Voici le main activity, ici s'affichera le Hello World
  */
@@ -32,35 +48,116 @@ public class LoginActivity extends AppCompatActivity {
         btnLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
-                boolean authen = connextionTest();
-
-                if (authen) {
-                    goToTasklistActivity();
-                    Log.d("testLogin", "réussi à se connecter");
-                } else {
-                    Toast.makeText(LoginActivity.this, " échec de connexion", Toast.LENGTH_LONG).show();
-                    Log.d("testLogin", "échouer à se connecter");
-                }
+            connextionTest(etIdentifiant.getText().toString(),etMdp.getText().toString());
             }
         });
     }
 
-    private void goToTasklistActivity() {
+   /*
+      -->   EXO
+    private void goToTasklistActivity(String token) {
         Intent intent = new Intent();
         intent.setClass(LoginActivity.this, TasklistActivity.class);
+        intent.putExtra("token",token);
         startActivity(intent);
     }
+    */
 
-    private boolean connextionTest() {
-        String nom_user = etIdentifiant.getText().toString();
-        String mdp = etMdp.getText().toString();
-        if (nom_user.equals("admin") && mdp.equals("123")) {
-            return true;
-        } else {
-            return false;
+    /**
+     * Permet de définir l'activité idéal de destination après la connexion
+     *
+     *
+     * @param token
+     */
+    public void goToAppropriateActivity(String token){
+        Intent intent = new Intent();
+        intent.putExtra("token",token);
+        setResult(RESULT_OK, intent);
+        if(this.getIntent().getBooleanExtra("isLoggedOut",false)){
+            //Redirection sur une activité par défaut si l'on revient d'une deconnexion
+            intent.setClass(this, TasklistActivity.class);
+            intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+            startActivity(intent);
         }
+        finish();
 
+    }
+
+    /**
+     * Fait appel à un web
+     */
+    private void connextionTest(final String login, final String passwd) {
+        RequestQueue queue = Volley.newRequestQueue(this);
+        String url = "http://lelabo228.com/txapi/index.php/connexion";
+        StringRequest postRequest = new StringRequest(Request.Method.POST, url,
+                new Response.Listener<String>()
+                {
+                    @Override
+                    public void onResponse(String response) {
+                        // response
+
+                        Gson gson = new Gson();
+                        String token =  gson.fromJson(response, Chauffeur[].class)[0].getToken();
+                        // goToTasklistActivity(token); --> EXO
+                        goToAppropriateActivity(token);
+
+                        Log.d("testLogin", "réussi à se connecter");
+                    }
+                },
+                new Response.ErrorListener()
+                {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        // error
+                        Toast.makeText(LoginActivity.this, " échec de connexion", Toast.LENGTH_LONG).show();
+                        Log.d("testLogin", "échouer à se connecter ");
+                    }
+                }
+        ) {
+            @Override
+            protected Map<String, String> getParams()
+            {
+                Map<String, String>  params = new HashMap<String, String>();
+                params.put("login", login);
+                params.put("passwd", hashToSHA512(passwd,""));
+                return params;
+            }
+        };
+        queue.add(postRequest);
+
+    }
+
+    /**
+     * Permet de hasher un mot de passe en SHA512
+     * @param passwordToHash le mot de passe à hasher
+     * @param salt le sel
+     * @return
+     */
+    public String hashToSHA512(String passwordToHash, String   salt){
+        String generatedPassword = null;
+        try {
+            MessageDigest md = MessageDigest.getInstance("SHA-512");
+            try {
+                md.update(salt.getBytes("UTF-8"));
+            } catch (UnsupportedEncodingException e) {
+                e.printStackTrace();
+            }
+            byte[] bytes = new byte[0];
+            try {
+                bytes = md.digest(passwordToHash.getBytes("UTF-8"));
+            } catch (UnsupportedEncodingException e) {
+                e.printStackTrace();
+            }
+            StringBuilder sb = new StringBuilder();
+            for(int i=0; i< bytes.length ;i++){
+                sb.append(Integer.toString((bytes[i] & 0xff) + 0x100, 16).substring(1));
+            }
+            generatedPassword = sb.toString();
+        }
+        catch (NoSuchAlgorithmException e){
+            e.printStackTrace();
+        }
+        return generatedPassword;
     }
 
 }
--- app/src/main/java/fr/utt/adrien/helloworld/TasklistActivity.java
+++ app/src/main/java/fr/utt/adrien/helloworld/TasklistActivity.java
@@ -1,18 +1,134 @@
 package fr.utt.adrien.helloworld;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+import org.json.JSONObject;
+
+import java.lang.reflect.Type;
+import java.util.ArrayList;
+import java.util.List;
+
+import fr.utt.adrien.helloworld.model.Livraison;
 
 /**
  * Created by qifan on 2016/11/30.
  */
 
-public class TasklistActivity extends AppCompatActivity {
+public class TasklistActivity extends RequestActivity {
 
+    private String TOKEN="";
+    private List<Livraison> livraisons = new ArrayList<>();
+    private ListView mLivraisonListView;
+    ArrayAdapter<Livraison> mAdapter;
+    private final int REQUEST_GET_MES_LIVRAISONS = 1;
+    private final int REQUEST_GET_SE_DECONNECTER = 2;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tasklist);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        TOKEN = getIntent().getStringExtra("token");
+        mLivraisonListView = (ListView) findViewById(R.id.livraisonListView);
+        mAdapter = new ArrayAdapter<Livraison>(this,android.R.layout.simple_list_item_1,livraisons);
+        mLivraisonListView.setAdapter(mAdapter);
+        getMesLivraisons();
+    }
+
+
+    public void getMesLivraisons(){
+        doGet(REQUEST_GET_MES_LIVRAISONS,"http://lelabo228.com/txapi/index.php/mes_livraisons/",TOKEN);
+       /*
+        ---> EXO
+       final String url = http://lelabo228.com/txapi/index.php/mes_livraisons/"+TOKEN;
+        RequestQueue queue = Volley.newRequestQueue(this);
+        StringRequest getRequest = new StringRequest(Request.Method.GET, url,
+                new Response.Listener<String>()
+                {
+                    @Override
+                    public void onResponse(String response) {
+                        Type listType = new TypeToken<List<Livraison>>() {}.getType();
+                        livraisons = new Gson().fromJson(response, listType);
+                        mAdapter.clear();
+                        mAdapter.addAll(livraisons);
+                        mAdapter.notifyDataSetChanged();
+                    }
+                },
+                new Response.ErrorListener()
+                {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Log.d("Error.Response : ",""+error.networkResponse.statusCode);
+                    }
+                }
+        );
+        queue.add(getRequest);*/
+    }
+
+    @Override
+    public void onOtherErrorResponse(int idRequest, int statusCode) {
+
+    }
+
+    @Override
+    public void onSuccess(int idRequest, String response) {
+        switch (idRequest){
+            case REQUEST_GET_MES_LIVRAISONS:
+                Type listType = new TypeToken<List<Livraison>>() {}.getType();
+                livraisons = new Gson().fromJson(response, listType);
+                mAdapter.clear();
+                mAdapter.addAll(livraisons);
+                mAdapter.notifyDataSetChanged();
+                break;
+            case REQUEST_GET_SE_DECONNECTER:
+                doLogout();
+                break;
+            default:
+                break;
+        }
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_task_list, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_logout:
+                doGet(REQUEST_GET_SE_DECONNECTER,"http://lelabo228.com/txapi/index.php/deconnexion/",TOKEN);
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    @Override
+    public void onBackFromLogin(String token) {
+        TOKEN = token;
+        getMesLivraisons();
     }
 }
